9081551f82e13c9273db5b82e9e9a9d53eec70fb
Tan Chin Sheng <tanchinsheng@gmail.com>
2015-01-25 17:08:51 +0800

    Added Security

--- sonar-training/tpms/tpms-java/src/tol/SQLExceptionConstraint.java
+++
@@ -1,8 +0,0 @@
-package tol;
-
-import java.sql.*;
-
-public class SQLExceptionConstraint extends SQLException
-{
-  public SQLExceptionConstraint(String msg) {super(msg);}
-}
--- sonar-training/tpms/tpms-java/src/tol/SQLExceptionLock.java
+++
@@ -1,8 +0,0 @@
-package tol;
-
-import java.sql.*;
-
-public class SQLExceptionLock extends SQLException
-{
-  public SQLExceptionLock(String msg) {super(msg);}
-}

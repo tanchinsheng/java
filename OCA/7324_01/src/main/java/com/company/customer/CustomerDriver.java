d0a7385f4de6b51564e7235232e3beec6d4b1eb8
Tan Chin Sheng <tanchinsheng@gmail.com>
2015-02-25 15:42:30 +0800

    Updated 7323_01

--- OCA/7324_01/src/main/java/com/company/customer/CustomerDriver.java
+++ OCA/7324_01/src/main/java/com/company/customer/CustomerDriver.java
@@ -6,8 +6,14 @@ public class CustomerDriver {
         // Define a reference and creates a new Customer object
         Customer customer;
         customer = new Customer();
-        //customer.balance = new BigDecimal(12506.45f);                
+        //customer.balance = new BigDecimal(12506.45f);
         customer.setBalance(12506.45f);
+        try {
+            customer.setName(null);
+        } catch (Exception e) {
+            System.out.println("Names must not be null");
+        }
+
         System.out.println(customer.toString());
     }
 }
